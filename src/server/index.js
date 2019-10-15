4351318f82ac2a028eac720f66570f75043201d6
ekoeryanto <ekoeryanto@gmail.com>
2019-10-15 23:00:57 +0700

    chore(server): better response

--- src/server/index.js
+++ src/server/index.js
@@ -6,93 +6,124 @@ import { json } from 'body-parser'
 
 import store from './store'
 
-export const folka = Polka()
+export const polka = Polka()
+
+function sendJSON (res, data, code = 200, headers = {}) {
+  res.writeHead(code, {
+    'Content-Type': 'application/json',
+    ...headers
+  })
+  res.end(JSON.stringify(data))
+}
 
 function authenticate (req, res, next) {
-  const pin = store.get('pin')
-  if (pin !== req.headers['x-pin']) {
-    res.writeHead(401, {
-      'Content-Type': 'application/json'
-    })
-    res.end(
-      JSON.stringify({
-        status: 401,
+  const pin = req.headers['x-pin']
+  if (pin !== store.get('pin')) {
+    sendJSON(
+      res,
+      {
+        status: 'fail',
         error: 'Unauthorized',
         message: 'You are not allowed to use the service'
-      })
+      },
+      401
     )
   }
-
-  req.PIN = pin
 
   next()
 }
 
-folka
+polka
   .use(json(), authenticate)
   .get('/printers', (req, res) => {
-    res.json(printer.getPrinters())
-  })
-  .get('/printer/:name', (req, res) => {
-    res.json(printer.getPrinter(req.params.name))
+    sendJSON(res, printer.getPrinters())
   })
   .get('/printer', (req, res) => {
-    res.json(printer.getPrinter(printer.getDefaultPrinterName()))
+    const name = req.query.name || printer.getDefaultPrinterName()
+    try {
+      sendJSON(res, printer.getPrinter(name))
+    } catch (error) {
+      sendJSON(res, { status: 'fail', error: 'BadRequest', message: error.toString() }, 400)
+    }
   })
   .get('/formats', (req, res) => {
-    res.json(printer.getSupportedPrintFormats())
+    sendJSON(res, printer.getSupportedPrintFormats())
   })
   .get('/commands', (req, res) => {
-    res.json(printer.getSupportedJobCommands())
+    sendJSON(res, printer.getSupportedJobCommands())
   })
-  .get('/job/:name/:id', (req, res) => {
-    res.json(printer.getJob(req.params.name, req.params.id))
+  .get('/job/:id', (req, res) => {
+    const name = req.query.printer || printer.getDefaultPrinterName()
+    sendJSON(res, printer.getJob(name, req.params.id))
   })
-  .post('/job/:name/:id', (req, res) => {
-    const { command } = req.body
-    const { name, id } = req.params
+  .post('/job/:id', (req, res) => {
+    const { command, printer: name } = req.body
+    const { id } = req.params
     const success = printer.setJob(name, id, command)
-    res.json({
-      name,
+    const data = {
+      printer,
       id,
       command,
       success
-    })
+    }
+    sendJSON(res, data, 201)
   })
-  .delete('/job/:name/:id', (req, res) => {
-    const { name, id } = req.params
-    const success = printer.setJob(name, id, 'CANCEL')
-    res.json({
-      name,
-      id,
-      success
-    })
+
+  .delete('/job/:id', (req, res) => {
+    const name = req.query.printer || printer.getDefaultPrinterName()
+    const { id } = req.params
+    try {
+      const success = printer.setJob(name, id, 'CANCEL')
+      sendJSON(res, { printer: name, id, success }, 201)
+    } catch (err) {
+      sendJSON(
+        res,
+        {
+          status: 'fail',
+          error: 'BadRequest',
+          message: err.message || err.code || err.toString()
+        },
+        400
+      )
+    }
   })
-  .get('/options/:name', (req, res) => {
-    res.json(printer.getPrinterDriverOptions(req.params.name))
+
+  .get('/options', (req, res) => {
+    const name = req.query.printer || printer.getDefaultPrinterName()
+    try {
+      sendJSON(res, printer.getPrinterDriverOptions(name))
+    } catch (error) {
+      sendJSON(res, { status: 'fail', error: 'BadRequest', message: error.toString() }, 400)
+    }
   })
   .post('/print', (req, res) => {
     printer.printDirect({
       ...req.body,
       success: jobId => {
-        res.json({
-          printer: req.body.name || printer.getDefaultPrinterName(),
-          jobId
-        })
+        sendJSON(
+          res,
+          {
+            printer: req.body.printer || printer.getDefaultPrinterName(),
+            jobId
+          },
+          201
+        )
       },
       error: err => {
-        const code = 400
-        res.status(code)
-          .json({
-            status: code,
+        sendJSON(
+          res,
+          {
+            status: 'fail',
             error: 'BadRequest',
             message: err.message
-          })
+          },
+          400
+        )
       }
     })
   })
 
-export const server = http.createServer(folka.handler)
+export const server = http.createServer(polka.handler)
 
 export function start () {
   const { ip, port } = store.get('network')
@@ -107,8 +138,19 @@ server.on('close', () => {
   store.set('listening', null)
 })
 
-ipcMain.on('server.stop', event => {
-  server.close(err => {
+ipcMain.on('server.stop', async event => {
+  try {
+    await new Promise((resolve, reject) => {
+      server.close(err => {
+        if (err) reject(err)
+        else resolve()
+      })
+    })
+    event.reply('server.stoped')
+  } catch (error) {
+    event.reply('server.error', error)
+  }
+  polka.close(err => {
     event.reply(err ? 'server.error' : 'server.stoped', err)
   })
 })
