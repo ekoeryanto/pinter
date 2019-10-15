f0d76898f73c21ece8febd9b053cfc0d88fb2623
ekoeryanto <ekoeryanto@gmail.com>
2019-10-15 23:41:29 +0700

    chore: prepare publish

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-template-curly-in-string */
 module.exports = {
   transpileDependencies: [
     // 'vuetify'
@@ -9,11 +10,48 @@ module.exports = {
       builderOptions: {
         appId: 'app.pake.pinter',
         productName: 'Pinter',
+        publish: ['github'],
         directories: {
           buildResources: 'res'
         },
+        nsis: {
+          artifactName: '${name}-${version}-bundle.${ext}'
+        },
+        nsisWeb: {
+          artifactName: '${name}-${version}-${arch}.${ext}'
+        },
+        rpm: {
+          depends: ['libcups2-devel']
+        },
+        deb: {
+          depends: ['gconf2', 'gconf-service', 'libnotify4', 'libappindicator1', 'libxtst6', 'libnss3', 'libcups2-dev']
+        },
         win: {
-          icon: 'icons/png'
+          icon: 'icons/png',
+          target: [
+            {
+              target: 'nsis-web',
+              arch: ['ia32', 'x64']
+            },
+            {
+              target: 'nsis',
+              arch: ['ia32', 'x64']
+            }
+          ]
+        },
+        linux: {
+          icon: 'icons/png',
+          category: 'Business',
+          target: [
+            {
+              target: 'deb',
+              arch: ['ia32', 'x64']
+            },
+            {
+              target: 'rpm',
+              arch: ['ia32', 'x64']
+            }
+          ]
         }
       }
     }
