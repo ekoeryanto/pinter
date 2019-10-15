a72c595b2fb557741d66a0c0d41fcc4887964aa2
ekoeryanto <ekoeryanto@gmail.com>
2019-10-15 23:01:34 +0700

    chore: server close on window-all-closed

--- src/background.js
+++ src/background.js
@@ -151,6 +151,7 @@ if (!appLock) {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
+      server.close()
       app.quit()
     }
   })
